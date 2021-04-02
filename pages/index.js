64af38b25e65917f2728192db8704eb4aad9704a
arehman03 <mianabdulrehman6@gmail.com>
2021-04-02 17:24:36 +0500

    initial by AR

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div class="main">
+      <div class="object">
+        <div class="object-rope"></div>
+        <div class="object-shape">
+          Coming <span class="soon">Soon</span>
+        </div>
+      </div>
+      <div class="content">
+        <img width="300" src='./Elias_business_-_PNG-01 (1).png' />
+        <h5 class="heading">Site is under construction</h5>
+      </div>
+    </div>
+  )
+}
+
